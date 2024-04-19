611ae028b687d61424919f35a834c0064fb8033c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-19 23:54:18 +0200

    fix a requirement issue

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/puppet
-# Puppet script to install a specific version of flask
+# Puppet script to install Flask version 2.1.0 using pip3
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+    ensure => installed,
+}
+
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    # Check package idempotency
+    unless  => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0"',
 }
