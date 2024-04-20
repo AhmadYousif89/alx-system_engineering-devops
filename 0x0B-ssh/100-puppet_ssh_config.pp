5182e2266d93cff0432013aee688d507ed2e9451
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-20 16:47:41 +0200

    setting up a ssh config file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
 # Setting up my client config file using puppet
 
-file_line { 'Turn off passwd auth':
+file_line { 'ignore passwd auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
+  line    => 'PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Delare identity file':
+file_line { 'set identity file':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school',
+  line    => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
