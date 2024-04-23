0563bba0780743b13a5fc7ebe66b09d3521e4774
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-23 13:36:16 +0200

    puppet script to install and configure nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,26 @@
 # Define the nginx configuration
 
-# Update package repositories
-exec { 'apt-update':
+exec { 'update system':
   command => '/usr/bin/apt-get update',
 }
 
-# Package installation and update
 package { 'nginx':
   ensure  => 'installed',
   require => Exec['update system'],
 }
 
-# Create index page
 file { '/var/www/html/index.html':
-  content => '
-    <!DOCTYPE html>
-      <html>
-        <head>
-        <title>My Web Server</title>
-        </head>
-        <body style="font-family: Open Sans;">
-          <h1 style="text-align: center; margin: 0; font-size: 5rem;">Hello World!</h1>
-        </body>
-      </html>',
+  content => 'Hello World!',
 }
 
-# Configure /redirect_me page
-exec { 'configure redirection':
-  ensure  => present,
-  command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=w_u4iY45A_U permanent;" /etc/nginx/sites-available/default',
+exec { 'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+service { 'nginx':
+  ensure  => running,
   require => Package['nginx'],
-  notify  => Service['nginx'],
 }
 
 # # Configure custom 404 page
@@ -59,8 +49,8 @@ exec { 'configure redirection':
 # }
 
 # Restart nginx service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
+# service { 'nginx':
+#   ensure  => 'running',
+#   enable  => true,
+#   require => Package['nginx'],
+# }
