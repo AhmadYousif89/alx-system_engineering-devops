11b705756c384af1b5e92d83f6a4dd8540e0ae6d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-30 02:10:57 +0300

    Puppet script that installs and configure a Ngnix web server and adds a custom header.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,10 @@
 # Puppet script that installs and configure a Ngnix web server and adds a custom header.
 
 exec { 'command':
-  command  => 'sudo apt-get update;
+  command  => '
+  sudo apt-get update;
   sudo apt-get install nginx -y
-  sudo sed -i "/^http {/a \\tadd_header X-Served-By \$hostname;" /etc/nginx/nginx.conf
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   sudo service nginx restart',
   provider => shell,
 }
