574f691e0e235428d0885afb292080113364c6de
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-16 19:23:07 +0300

    Fix content typo in Puppet script

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Puppet script to create a file in the /tmp directory
 
 file { '/tmp/school':
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
