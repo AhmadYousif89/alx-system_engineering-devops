d2f4128aa80035c816fa1263cd31fc42132b86e5
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 19:12:43 +0300

    Add top_ten function to query and print titles of hot posts from Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Query the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": ""}
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code > 399:
+        print(None)
+        return
+    for post in response.json().get('data', {}).get('children', []):
+        print(post.get('data', {}).get('title', ''))
