b74463b5d91200c9351b3d65b153bec5eeeebffe
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 19:44:22 +0300

    Refactor number_of_subscribers function to improve readability and error handling

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-
-"""This module contains number_of_subscribers functions"""
-
+"""
+Query the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers (not active users, total subscribers)
-    for a given subreddit.
-    """
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/125.0.0.0 Safari/537.36"
-    }
-
+    """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/125.0.0.0"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if response.status_code > 399:
         return 0
+    return response.json().get('data', {}).get('subscribers', 0)
