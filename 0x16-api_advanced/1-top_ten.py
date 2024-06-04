38d73d76220626dad90f6e225c6f6608e1dfc750
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 21:01:05 +0300

    Remove unnecessary headers in API request

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,7 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/125.0.0.0"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code > 399:
         print(None)
         return
