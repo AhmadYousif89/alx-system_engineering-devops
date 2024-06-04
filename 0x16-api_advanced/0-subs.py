5354e91b317913e0c9aeb8e2b1686402a4f3dd60
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 18:24:02 +0300

    Handle redirects when making API request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         total_subscribers = response.json().get('data').get('subscribers')
         return total_subscribers
