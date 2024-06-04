3bec938e3c4e9610a2cab91d07ad4f4d88688a15
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 18:21:18 +0300

    Refactor number_of_subscribers function to return the number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,20 +3,21 @@
 Query the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url)
     if response.status_code == 200:
         total_subscribers = response.json().get('data').get('subscribers')
-        print(total_subscribers)
+        return total_subscribers
     else:
-        print(0)
+        return 0
 
 
 if __name__ == "__main__":
-    import requests
     from sys import argv
 
     subreddit = argv[1] if len(argv) > 1 else ''
