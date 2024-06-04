1af5d976d243aa2da693d45e75f67a5491302687
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-05 00:16:42 +0300

    Update limit parameter in recurse function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,13 +9,15 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     """Return a list of titles of all hot articles for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {"limit": 50, "after": after}
+    print(hot_list)
+    params = {"limit": 100, "after": after}
     response = requests.get(url, params=params, allow_redirects=False)
     if response.status_code > 399:
         return None
     posts = response.json().get('data', {}).get('children', [])
     for post in posts:
-        hot_list.append(post.get('data', {}).get('title', ''))
+        title = post.get('data', {}).get('title', '')
+        hot_list.append(title)
     after = response.json().get('data', {}).get('after', None)
     if after:
         return recurse(subreddit, hot_list, after)
