46aacbcbc440f8fcea25c980da71d8ce4d993e0a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 19:44:29 +0300

    Fix User-Agent header in top_ten function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,13 +11,12 @@ def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/125.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/125.0.0.0"
     }
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code > 399:
         print(None)
         return
-    for post in response.json().get('data', {}).get('children', []):
+    posts = response.json().get('data', {}).get('children', [])
+    for post in posts:
         print(post.get('data', {}).get('title', ''))
