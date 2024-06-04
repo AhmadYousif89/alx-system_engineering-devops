a6c6b11d1100c48370fee060b774fb9f712b7bda
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 18:31:48 +0300

    Add User-Agent header to API request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,8 @@ def number_of_subscribers(subreddit):
     if not subreddit:
         return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     total_subscribers = response.json().get('data', {}).get('subscribers', 0)
     return total_subscribers
 
