929a2c080ca00a9263d6ee0145d6baf44836c2bd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 18:30:34 +0300

    Refactor number_of_subscribers function to handle empty subreddit input

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,22 +8,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        total_subscribers = response.json().get('data').get('subscribers')
-        return total_subscribers
-    else:
+    if not subreddit:
         return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url)
+    total_subscribers = response.json().get('data', {}).get('subscribers', 0)
+    return total_subscribers
 
 
 if __name__ == "__main__":
     from sys import argv
 
     subreddit = argv[1] if len(argv) > 1 else ''
-    if not subreddit:
-        print("Usage: {} <subreddit>".format(argv[0]))
-        exit(1)
-
     number_of_subscribers(subreddit)
