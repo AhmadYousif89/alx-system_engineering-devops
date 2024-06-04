7b1a08acf6829865b4b5899352ea346385799857
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-04 18:39:05 +0300

    Refactor API request in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code > 399:
         return 0
     return response.json().get('data', {}).get('subscribers', 0)
-
-
-if __name__ == "__main__":
-    from sys import argv
-
-    subreddit = argv[1] if len(argv) > 1 else ''
-    number_of_subscribers(subreddit)
