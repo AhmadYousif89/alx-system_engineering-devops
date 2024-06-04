b2a4039f5746de8e1002c29ee9f5dd15d1cf256f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-05 00:30:07 +0300

    Fix count_words function to correctly count occurrences of words in post titles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,17 +14,16 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
     if response.status_code > 399:
         return None
     data = response.json().get('data', {})
-    childs = data.get('children', [])
-    for post in childs:
-        title = post["data"]["title"]
+    posts = data.get('children', [])
+    for post in posts:
+        title = post.get('data', {}).get('title', '')
         hot_list.append(title)
     after = data.get('after', None)
     if after:
-        count_words(subreddit, word_list, hot_list, after)
-    else:
-        word_dict = {}
-        for title in hot_list:
-            for word in word_list:
-                word_dict[word] += title.lower().split(' ').count(word.lower())
-        for k, v in sorted(word_dict.items(), key=lambda w: (-w[1], w[0])):
-            print("{}: {}".format(k, v))
+        return count_words(subreddit, word_list, hot_list, after)
+    word_dict = {}
+    for title in hot_list:
+        for word in word_list:
+            word_dict[word] += title.lower().split().count(word.lower())
+    for k, v in sorted(word_dict.items(), key=lambda w: (-w[1], w[0])):
+        print("{}: {}".format(k, v))
