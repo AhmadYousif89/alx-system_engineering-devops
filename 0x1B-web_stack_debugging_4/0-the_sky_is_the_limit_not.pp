c7f81642c900d4e5641890c0eb0ffe672a82adc7
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-13 14:00:55 +0300

    Update ulimit value in /etc/default/nginx file to fix failing requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Update the ulimit value inside the /etc/default/nginx file to fix the issue with the failling requests.
 
 exec { 'update_nginx_ulimit':
-  command => 'sed -i "s/^ulimit=.*/ULIMIT=\"-n 256\"/Ig" /etc/default/nginx',
+  command => 'sed -i "s/^ulimit=.*/ULIMIT=\"-n 4096\"/Ig" /etc/default/nginx',
   path    => '/bin:/usr/bin:/usr/local/bin',
 }
 
